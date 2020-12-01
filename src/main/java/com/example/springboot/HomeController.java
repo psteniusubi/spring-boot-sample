c2b9cf6fa6ae70c50756a62ba5997e89d8b4ad07
petteri.stenius@ubisecure.com <petteri.stenius@ubisecure.com>
2020-12-01 19:38:24 +0200

    updates

--- src/main/java/com/example/springboot/HomeController.java
+++ src/main/java/com/example/springboot/HomeController.java
@@ -1,11 +1,14 @@
 package com.example.springboot;
 
+import javax.annotation.security.RolesAllowed;
+
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RolesAllowed("ROLE_USER")
 @Controller
 public class HomeController {
 	@GetMapping("/")
